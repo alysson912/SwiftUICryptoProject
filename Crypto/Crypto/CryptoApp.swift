ddf0c002b942820084cc079b8b68706fc64aa7e9
alysson912 <almenezes912@gmail.com>
2024-12-12 08:34:29 -0300

    start project

---
+++ Crypto/Crypto/CryptoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoApp.swift
+//  Crypto
+//
+//  Created by Alysson Menezes on 11/12/24.
+//
+
+import SwiftUI
+
+@main
+struct CryptoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
