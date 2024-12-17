cf7bdc87dc412ae19a2e864bc3f784b5f98188d4
alysson912 <almenezes912@gmail.com>
2024-12-17 19:04:01 -0300

    Design a row to display coins in a List, foi criada uma extension Double para converter o valor da moeda para a casa decimal desejada,

---
+++ Crypto/Crypto/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,18 @@
+//
+//  CoinRowView.swift
+//  Crypto
+//
+//  Created by Alysson Menezes on 17/12/24.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CoinRowView()
+}
