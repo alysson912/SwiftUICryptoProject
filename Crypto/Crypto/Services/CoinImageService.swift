73809729e738877ae1fbb52b65c8762dc658d4e6
Alysson Menezes Dodo <almenezes912@gmail.com>
2025-01-03 12:13:15 -0300

    Saving images to the File Manager,  baixando imagens da API para o app, criando gerenciador de arquivos e salvando as imagens localmente

--- Crypto/Crypto/Services/CoinImageService.swift
+++ Crypto/Crypto/Services/CoinImageService.swift
@@ -12,25 +12,44 @@ import Combine
 class CoinImageService {
     
     @Published var image: UIImage? = nil
-   private var imageSubscription: AnyCancellable?
+    
+    private var imageSubscription: AnyCancellable?
     private let coin: CoinModel
+    private let fileManager = LocalFileManager.instance
+    private let folderName = "coin_images"
+    private let imageName: String
+    
     
     init(coin: CoinModel) {
         self.coin = coin
+        self.imageName = coin.id
         getCoinImage()
     }
     
     private func getCoinImage() {
+        if let savedImage = fileManager.getImage(imageName: imageName, folderName: folderName){
+            image = savedImage
+            print("Retrieved image from File Manager!")
+        } else {
+            downloadCoinImage()
+            print("Downloading image now")
+        }
+    }
+    
+    private func downloadCoinImage() {
+        print("Downloading image now")
         guard let url = URL( string: coin.image) else { return }
         // Download Data
         imageSubscription = NetWorkingManager.download(url: url)
             .tryMap({ (data) -> UIImage? in
                 return UIImage(data: data)
             })
-    
-            .sink(receiveCompletion: NetWorkingManager.handleCompletion, receiveValue: { [weak self] (retunedImage) in
-                self?.image = retunedImage
-                self?.imageSubscription?.cancel()
+        
+            .sink(receiveCompletion: NetWorkingManager.handleCompletion, receiveValue: { [weak self] (returnedImage) in
+                guard let self = self, let downloadedImage = returnedImage else { return }
+                self.image = downloadedImage
+                self.imageSubscription?.cancel()
+                self.fileManager.saveImage(image: downloadedImage, imageName: self.imageName, folderName: self.folderName)
             })
     }
 }
